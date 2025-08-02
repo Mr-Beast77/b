c1571c259fc405eb7be1888511c9baae0e6c1864
Mr-Beast77 <71363575+Mr-Beast77@users.noreply.github.com>
2025-08-02 14:50:46 +0600

    Update b.py

--- b.py
+++ b.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
-print(3)
 from helium import *
+from selenium.webdriver import ChromeOptions
+import os
+from datetime import datetime
+from time import sleep
+
+options = ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+start_chrome(options=options)
+u = 'https://www.google.com'
+go_to(u)
+title = Window().title
+print(title)
